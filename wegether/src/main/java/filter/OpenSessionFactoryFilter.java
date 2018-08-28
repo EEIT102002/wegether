529010bf9076a114e2a2a0051134a8420dfc65fd
蕭遠平 <eeit10204@>
2018-08-28 11:03:38 +0800

    version1.0
    version1.0

---
+++ wegether/src/main/java/filter/OpenSessionFactoryFilter.java
@@ -0,0 +1,46 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+
+@WebFilter("/*")
+public class OpenSessionFactoryFilter implements Filter {
+	
+	private SessionFactory sessionFactory;
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {	
+		try {
+			
+			sessionFactory.getCurrentSession().beginTransaction();
+			chain.doFilter(request, response);
+			sessionFactory.getCurrentSession().getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			sessionFactory.getCurrentSession().getTransaction().rollback();
+			chain.doFilter(request, response);		
+		}
+	}
+
+	@Override
+	public void destroy() {	
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		ApplicationContext context = (ApplicationContext)arg0.getServletContext()
+				 .getAttribute(
+						 WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+		sessionFactory = (SessionFactory)context.getBean("sessionFactory");
+		
+	}
+
+}
