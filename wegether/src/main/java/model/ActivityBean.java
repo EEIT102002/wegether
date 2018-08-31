fa93b6650fbbcbc1513fe1e086b8b7d0c4fde7ae
吳宗諭 <eeit1022@iii.com>
2018-08-31 11:10:16 +0800

    mapping ok
    mapping ok

--- wegether/src/main/java/model/ActivityBean.java
+++ wegether/src/main/java/model/ActivityBean.java
@@ -58,111 +58,86 @@ public class ActivityBean {
 		public void setMemberBean(MemberBean memberBean) {
 			this.memberBean = memberBean;
 		}
-		/*
-	//picture
-	@OneToMany(
-			mappedBy="activityBean",
-			cascade= {CascadeType.REMOVE}
-			)
-	private Set<PictureBean> pictureBean;	
-	public Set<PictureBean> getPictureBean() {
-		return pictureBean;
-	}
-	public void setPictureBean(Set<PictureBean> pictureBean) {
-		this.pictureBean = pictureBean;
-	}
-	
-	//article
-	@OneToMany(
-			mappedBy="articleBean",
-			cascade= {CascadeType.REMOVE}
-			)
-	private Set<ArticleBean> articleBean;	
-	public Set<ArticleBean> getArticleBean() {
-		return articleBean;
-	}
-	public void setArticleBean(Set<ArticleBean> articleBean) {
-		this.articleBean = articleBean;
-	}
-	
-	//msg
-	@OneToMany(
-			mappedBy="msgBean",
-			cascade= {CascadeType.REMOVE}
-			)
-	private Set<MsgBean> msgBean;	
-	public Set<MsgBean> getMsgBean() {
-		return msgBean;
-	}
-	public void setMsgBean(Set<MsgBean> msgBean) {
-		this.msgBean = msgBean;
-	}
-	
-	//attend
-	@OneToMany(
-			mappedBy="attendBean",
-			cascade= {CascadeType.REMOVE}
-			)
-	private Set<AttendBean> attendBean;
-	public Set<AttendBean> getAttendBean() {
-		return attendBean;
-	}
-	public void setAttendBean(Set<AttendBean> attendBean) {
-		this.attendBean = attendBean;
-	}
-	
-	//invite
-	@OneToMany(
-			mappedBy="inviteBean",
-			cascade= {CascadeType.REMOVE}
-			)
-	private Set<InviteBean> inviteBean;
-	public Set<InviteBean> getInviteBean() {
-		return inviteBean;
-	}
-	public void setInviteBean(Set<InviteBean> inviteBean) {
-		this.inviteBean = inviteBean;
-	}
-	
-	//notice
-	@OneToMany(
-			mappedBy="noticeBean",
-			cascade= {CascadeType.REMOVE}
-			)
-	private Set<NoticeBean> noticeBean;
-	public Set<NoticeBean> getNoticeBean() {
-		return noticeBean;
-	}
-	public void setNoticeBean(Set<NoticeBean> noticeBean) {
-		this.noticeBean = noticeBean;
-	}
-	
-	//trackactivity
-	@OneToMany(
-			mappedBy="trackactivityBean",
-			cascade= {CascadeType.REMOVE}
-			)
-	private Set<TrackactivityBean> trackactivityBean;
-	public Set<TrackactivityBean> getTrackactivityBean() {
-		return trackactivityBean;
-	}
-	public void setTrackactivityBean(Set<TrackactivityBean> trackactivityBean) {
-		this.trackactivityBean = trackactivityBean;
-	}
-
-	//activityclass
-	@OneToMany(
-			mappedBy="activityclassBean",
-			cascade= {CascadeType.REMOVE}
-			)
-	private Set<ActivityclassBean> activityclassBean;
-	public Set<ActivityclassBean> getActivityclassBean() {
-		return activityclassBean;
-	}
-	public void setActivityclassBean(Set<ActivityclassBean> activityclassBean) {
-		this.activityclassBean = activityclassBean;
-	}	
-	*/
+		
+//	//picture
+//	@OneToMany(
+//			mappedBy="activityBean",
+//			cascade= {CascadeType.REMOVE}
+//			)
+//	private Set<PictureBean> pictureBean;	
+//	public Set<PictureBean> getPictureBean() {
+//		return pictureBean;
+//	}
+//	public void setPictureBean(Set<PictureBean> pictureBean) {
+//		this.pictureBean = pictureBean;
+//	}
+//	
+//	//article
+//	@OneToMany(
+//			mappedBy="articleBean",
+//			cascade= {CascadeType.REMOVE}
+//			)
+//	private Set<ArticleBean> articleBean;	
+//	public Set<ArticleBean> getArticleBean() {
+//		return articleBean;
+//	}
+//	public void setArticleBean(Set<ArticleBean> articleBean) {
+//		this.articleBean = articleBean;
+//	}
+//	
+//	//msg
+//	@OneToMany(
+//			mappedBy="msgBean",
+//			cascade= {CascadeType.REMOVE}
+//			)
+//	private Set<MsgBean> msgBean;	
+//	public Set<MsgBean> getMsgBean() {
+//		return msgBean;
+//	}
+//	public void setMsgBean(Set<MsgBean> msgBean) {
+//		this.msgBean = msgBean;
+//	}
+//	
+//	//attend
+//	@OneToMany(
+//			mappedBy="attendBean",
+//			cascade= {CascadeType.REMOVE}
+//			)
+//	private Set<AttendBean> attendBean;
+//	public Set<AttendBean> getAttendBean() {
+//		return attendBean;
+//	}
+//	public void setAttendBean(Set<AttendBean> attendBean) {
+//		this.attendBean = attendBean;
+//	}
+//	
+//	//invite
+//	@OneToMany(
+//			mappedBy="inviteBean",
+//			cascade= {CascadeType.REMOVE}
+//			)
+//	private Set<InviteBean> inviteBean;
+//	public Set<InviteBean> getInviteBean() {
+//		return inviteBean;
+//	}
+//	public void setInviteBean(Set<InviteBean> inviteBean) {
+//		this.inviteBean = inviteBean;
+//	}
+//	
+//	//notice
+//	@OneToMany(
+//			mappedBy="noticeBean",
+//			cascade= {CascadeType.REMOVE}
+//			)
+//	private Set<NoticeBean> noticeBean;
+//	public Set<NoticeBean> getNoticeBean() {
+//		return noticeBean;
+//	}
+//	public void setNoticeBean(Set<NoticeBean> noticeBean) {
+//		this.noticeBean = noticeBean;
+//	}
+//	
+//	
 	
 	@Override
 	public String toString() {
