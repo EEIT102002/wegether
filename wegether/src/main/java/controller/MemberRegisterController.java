22ea2c988fdd228259aab385c3aa0b341e1ec34f
胡安邦 <benben3607@gmail.com>
2018-09-10 17:25:38 +0800

    更新

--- wegether/src/main/java/controller/MemberRegisterController.java
+++ wegether/src/main/java/controller/MemberRegisterController.java
@@ -1,29 +1,45 @@
 package controller;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@WebServlet("/MemberRegisterController")
+import model.MemberBean;
+import model.ServiceBean;
+import model.dao.implement.MemberInfoDAOHibernate;
+
+
+@Controller
 public class MemberRegisterController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private MemberInfoDAOHibernate memberInfoDAOHibernate;
    
-    public MemberRegisterController() {
-       
-    }
+	@InitBinder
+	public void registerPropertyEditor(WebDataBinder webDataBinder) {
+		webDataBinder.registerCustomEditor(java.util.Date.class,
+				new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), true));
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		;
 	}
-
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-		doGet(request, response);
-	}
+	@RequestMapping(
+			path= {"/register.controller"}
+		)
+public String selectMethod(Model model,MemberBean bean) {
+	List<MemberBean> result = (List<MemberBean>) memberInfoDAOHibernate.select(4);
+//	model.addAttribute("select", result);
+	return "register.success";
+}
 
 }
