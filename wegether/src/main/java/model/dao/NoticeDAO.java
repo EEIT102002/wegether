42219960632b27b0b84ee2685bf697d3e3c0a077
蕭遠平 <eeit10204@>
2018-09-11 20:23:40 +0800

    add trackmember blacklist search
    add trackmember blacklist search

--- wegether/src/main/java/model/dao/NoticeDAO.java
+++ wegether/src/main/java/model/dao/NoticeDAO.java
@@ -8,4 +8,8 @@ public interface NoticeDAO {
 	public abstract List<NoticeBean> selectByMemberId(Integer id, Integer first);
 	public abstract List<NoticeBean> selectByActivityId(Integer id, Integer ntype);
 	public abstract List<NoticeBean> selectByArticleId(Integer id, Integer ntype);
+	public abstract List<NoticeBean> selectByFriendId(Integer id, Integer ntype);
+	public abstract List<NoticeBean> selectByInviteId(Integer id, Integer ntype);
+	public abstract List<NoticeBean> selectByAttendId(Integer id, Integer ntype);
+
 }
